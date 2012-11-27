a420aefea00955d74c5f35b9e97117eb4bac075a
Joshua Tai <joshua.tai0907@gmail.com>
2012-11-27 23:04:09 +0800

    modify path

--- library/main.php
+++ library/main.php
@@ -6,15 +6,15 @@ if(eregi("MSIE 7.0",$_SERVER["HTTP_USER_AGENT"]) || eregi("MSIE 6.0",$_SERVER["H
 }*/
 $__DIR__= dirname(__FILE__);
 //引入no-catch程式;
-include($__DIR__."/../lib/no-cache.php");
+include($__DIR__."/../library/no-cache.php");
 //引入資料庫連結程式，並回傳狀態;
-require_once($__DIR__."/../lib/link.php");
+require_once($__DIR__."/../library/link.php");
 //載入functin;
-require_once ($__DIR__."/../lib/data_class.php");
+require_once ($__DIR__."/../library/data_class.php");
 
-require_once ($__DIR__."/../lib/page_class.php");
+require_once ($__DIR__."/../library/page_class.php");
 //載入變數定義檔;
-require_once ($__DIR__."/../lib/variable.php");
+require_once ($__DIR__."/../library/variable.php");
 //檢查變數合法;
 check(urlencode("您輸入的字元有含非法的字元！"));
 $_POST=slashes();
