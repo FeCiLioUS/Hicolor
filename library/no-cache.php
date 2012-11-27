ee0d076362f4b41d3db087f726c161e7aea806dd
Joshua Tai <joshua.tai0907@gmail.com>
2012-11-28 00:42:59 +0800

    d

--- library/no-cache.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Cache-Control: no-cache");
-header("Pragma: no-cache");
-header("Expires: Tue, Jan 12 1999 05:00:00 GMT");
-?>
--- library/variable.php
+++
@@ -1,4 +0,0 @@
-<?php
-   //系統時間;
-   $CRTE_TIME=mktime(date("H"),date("i"),date("s"),date("m"),date("d"),date("Y"));
-?>
