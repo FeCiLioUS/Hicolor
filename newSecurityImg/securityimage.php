656c1d667050f733cf4151c6836f02621230ef81
Joshua Tai <joshua.tai0907@gmail.com>
2013-01-05 13:40:38 +0800

    modify

--- newSecurityImg/securityimage.php
+++ newSecurityImg/securityimage.php
@@ -4,10 +4,10 @@ require_once($__DIR__."/../lib/no-cache.php");
 //引入資料庫連結程式，並回傳狀態;
 ob_start();
 session_start();
-require_once($__DIR__."../lib/link.php");
+require_once($__DIR__."/../lib/link.php");
 //Select random background image
 $bgurl = rand(1, 3);
-$im = "images/bg".$bgurl.".png";
+$im = $__DIR__."/images/bg".$bgurl.".png";
 //Generate the random string
 $chars = array("a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J",
 			   "k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T",
@@ -16,6 +16,7 @@ $textstr = '';
 for ($i = 0, $length = 5; $i < $length; $i++) {
    $textstr .= $chars[rand(0, count($chars) - 1)];
 }
+
 // SET SESSION VARIABLE
 $hashtext = md5($textstr);
 $tmpname = $hashtext.'.png';
@@ -37,18 +38,19 @@ $gravity = $gravities[rand(0, count($gravities)-1)];
 $angle = rand(-5, 5);
 
 # prepare our image magick command
-$cmd  = '/usr/bin/convert';
-$cmd .= ' -font "'.$font.'"';
-$cmd .= ' -fill "#'.$color.'"';
-$cmd .= ' -pointsize '.$size;
-$cmd .= ' -gravity "'.$gravity.'"';
-$cmd .= ' -draw \'text 0,0 "'.$textstr.'"\'';
-$cmd .= ' -rotate '.$angle;
-$cmd .= ' ./'.$im.' ./images/tmp/'.$tmpname;
+$cmd  = "convert";
+$cmd .= " -font \"".$font."\"";
+$cmd .= " -fill \"#".$color."\"";
+$cmd .= " -pointsize ".$size;
+$cmd .= " -gravity \"".$gravity."\"";
+$cmd .= " -draw \"text ".rand(0,50).",".rand(-5,5)." '".$textstr."'\"";
+$cmd .= " -rotate ".$angle;
+$cmd .= " ".$im." ".$__DIR__."\\images\\tmp\\".$tmpname;
 exec($cmd);
+
 // Output PNG Image
 header("Content-Type: image/png");
-$insert="insert into session_value (SE_COOKIE,SE_SESSION,SE_ORG) values(\"".$_SESSION[se_COOKIE]."\",\"".$_SESSION['strSec']."\",\"".$textstr."\")";
+$insert="insert into session_value (SE_COOKIE,SE_SESSION,SE_ORG) values(\"".$_SESSION['se_COOKIE']."\",\"".$_SESSION['strSec']."\",\"".$textstr."\")";
 mysql_query($insert);
 //echo $_COOKIE[se_COOKIE];
   //echo $insert."<br>";
@@ -57,8 +59,8 @@ mysql_query($insert);
 //echo $textstr."\n";
 //echo md5($textstr);
 //
-print fread(fopen('./images/tmp/'.$tmpname, 'r'), filesize('./images/tmp/'.$tmpname));
+print fread(fopen($__DIR__.'/images/tmp/'.$tmpname, 'r'), filesize($__DIR__.'/images/tmp/'.$tmpname));
 # since we've displayed the image lets go ahead and delete the actual file
-exec('rm -f ./images/tmp/'.$tmpname);
+exec('del '.$__DIR__.'\\images\\tmp\\'.$tmpname);
 ob_end_flush();
 ?>
