257d683bc9e30ff325c5485eef4d8b6b61ab70d1
Joshua Tai <joshua.tai0907@gmail.com>
2013-01-06 23:56:05 +0800

    modify

--- pages/admin.php
+++ pages/admin.php
@@ -1,8 +1,8 @@
 <?php
 $__DIR__= dirname(__FILE__);
 require_once($__DIR__."/../middleware/hicolor.php");
-$hicolor= new Hicolor();
-$hicolor-> assign("pageTitle", "最新消息");
-$hicolor-> assign("contentPath", "../templates/about.tpl"); 
+$hicolor= new Hicolor('admin', true);
+$hicolor-> assign("pageTitle", "管理系統");
+$hicolor-> assign("contentPath", "../templates/admin.tpl");
 $hicolor-> display("standard_template.tpl");
 ?>
