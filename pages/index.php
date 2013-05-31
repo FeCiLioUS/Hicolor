9577b6fb317d87afa0f055f7eca3992395a8fee6
Joshua Tai <joshua.tai0907@gmail.com>
2013-05-31 21:17:30 +0800

    #redirect to about page

--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,3 @@
 <?php
-$__DIR__= dirname(__FILE__);
-require_once($__DIR__."/../middleware/hicolor.php");
-$hicolor= new Hicolor();
-$hicolor-> assign("pageTitle", "最新消息");
-$hicolor-> assign("contentPath", "../templates/about.tpl"); 
-
-$hicolor-> display("standard_template.tpl");
+header("Location:../pages/about.php");
 ?>
